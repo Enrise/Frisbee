94d80c162da1821cedf9cf55d80b1dbfd89caab0
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2016-03-31 20:06:48 +0200

    Router for internal server: php -S localhost:8080 dev/router.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-chdir(dirname(__DIR__ . '..' . DIRECTORY_SEPARATOR));
+//chdir(dirname(__DIR__ . '..' . DIRECTORY_SEPARATOR));
 
 throw new \Frisbee\Application('myApp');
