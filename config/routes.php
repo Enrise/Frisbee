47ebbec465b148aeab2920369a1127c7780ff983
Nico Stapelbroek <nstapelbroek@gmail.com>
2016-03-31 23:33:16 +0200

    Allow implementing multiple actions in a controller by resolving actions from routes

--- config/routes.php
+++ config/routes.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Route;
  * Bleeding edge routing configuration
  */
 $routes = new RouteCollection();
-$routes->add('home_index', new Route('/', array('controller' => 'Index')));
-$routes->add('about_index', new Route('/about', array('controller' => 'About')));
+$routes->add('home_index', new Route('/', array('controller' => 'index')));
+$routes->add('home_code', new Route('/code-example', array('controller' => 'index', 'action' => 'codeExample')));
 
 return $routes;
