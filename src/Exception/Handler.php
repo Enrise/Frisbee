7068072e4d1d597285bdac30609fb68037b3344e
Stefan van Essen <git@stefan-van-essen.nl>
2016-03-31 22:00:20 +0200

    Added Cowsay exception handling

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -2,7 +2,10 @@
 
 namespace Frisbee\Exception;
 
+use Cowsayphp\Cow;
 use Exception;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 class Handler implements HandlerInterface
 {
@@ -15,8 +18,16 @@ class Handler implements HandlerInterface
                 exit; // No more exceptions, we are done.
             } catch (Flingable $e) {
                 self::handle($e);
-            } catch (\Exception $e) {
-                die($e->getMessage());
+            } catch (Exception $e) {
+                $loader = new Twig_Loader_Filesystem(APP_ROOT . '/resources/views');
+                $twig = new Twig_Environment($loader);
+                echo $twig->render(
+                    'error.twig',
+                    [
+                        'message' => Cow::say($e->getMessage()),
+                    ]
+                );
+                die;
             }
         }
     }
